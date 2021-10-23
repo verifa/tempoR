da0de523338b04768d4e5c9756137a3906ad9c8e
Bosse Nyström <bon@praqma.net>
2021-10-23 10:16:07 +0200

    Fix #47 - adds a simple shiny server test script

---
+++ shiny/shiny.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SHINY_DOCS="tempo.html"
+
+if [ ! -f "../tempo.html" ]; then
+  pushd ..
+  make
+  popd
+fi
+
+for doc in ${SHINY_DOCS}; do
+  cp ../${doc} shinyapps/.
+done
+
+docker run --rm -p 3838:3838 \
+    -v ${PWD}/shinyapps/:/srv/shiny-server/ \
+    -u shiny \
+    docker.io/rocker/shiny-verse
+
