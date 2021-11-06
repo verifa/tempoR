7a0c1da31a375da8583a8b9ac5667f37bf5c31a2
Bosse Nyström <bon@praqma.net>
2021-11-06 19:31:35 +0100

    Fix #57 - adds capword function to capitalise words

--- r-helpers.R
+++ r-helpers.R
@@ -19,3 +19,11 @@ check.packages <- function(package.list) {
              })
   }
 }
+
+capwords <- function(s, strict = FALSE) {
+  cap <- function(s) paste(toupper(substring(s, 1, 1)),
+                           {s <- substring(s, 2); if(strict) tolower(s) else s},
+                           sep = "", collapse = " " )
+  sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
